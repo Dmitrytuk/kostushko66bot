7ed2611e1e05b853b10f649707f6cddec5ac051b
Dmitry Ivanov <ydimail@yandex.ru>
2024-02-14 20:31:00 +0300

    prepare main for deploy. Clean comments and add production chat id

--- project/__main__.py
+++ project/__main__.py
@@ -2,7 +2,6 @@ import asyncio
 from dotenv import load_dotenv
 from os import getenv
 from pyrogram import Client, filters
-from pyrogram.types import BotCommand, ReplyKeyboardMarkup, ReplyKeyboardMarkup
 
 import logging
 
@@ -14,23 +13,12 @@ api_hash = getenv('api_hash')
 bot_token = getenv('bot_token')
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-# app = Client("my_account", api_id=api_id, api_hash=api_hash)
 
-# TARGET = -1001666747721
 TARGET = -4142900821
+# OCC gooup
+#TARGET = -1818883697
 
-MESSAGE = "{}, Whelcome to group!"
-
-# @app.on_message()
-# async def get_chat_id(client, message):
-#     print(message)
-
-# markup = ReplyKeyboardMarkup(
-#     keyboard=[],
-#     resize_keyboard=True,
-#     mute=True
-# )
-
+MESSAGE = "{}, Welcome to group!"
 
 @app.on_message(filters.chat(TARGET) & filters.new_chat_members)
 async def welcome(client, message):
@@ -41,15 +29,6 @@ async def welcome(client, message):
                               text,
                               disable_web_page_preview=True,
                               disable_notification=True)
-
-
-# bot_commands = [
-#         BotCommand("start", "Start the bot")
-#         ]
-
-# async def set_commands():
-#     async with app:
-#         await app.set_bot_commands(bot_commands)
 
 
 logging.basicConfig(
